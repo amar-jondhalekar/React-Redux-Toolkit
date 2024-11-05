c98c011552c94174787a137355bec054777611a7
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-11-05 10:17:13 +0530

    we can use the React-Redux hooks to let React components interact with the Redux store. We can read data from the store with useSelector, and dispatch actions using useDispatch. Create a src/features/counter/Counter.js file with a <Counter> component inside, then import that component into App.js and render it inside of <App>

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { useDispatch, useSelector } from 'react-redux'
 import './App.css'
+import { decrement, increment } from './features/couter/couterSlice';
 
 function App() {
-
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+  function handleIncrementClick(){
+    dispatch(increment());
+  }
+  function handleDecrementClick(){
+    dispatch(decrement());
+  }
   return (
     <>
-     <div>
-      Hello 
+     <div className='container'>
+        <button onClick={handleIncrementClick}> + </button>
+        <p>Count: {count}</p>
+        <button onClick={handleDecrementClick}> - </button>
      </div>
     </>
   )
