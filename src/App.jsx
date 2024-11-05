6e2da2fd4190665cbb3d45eef9f5dd3242196e77
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-11-05 10:40:45 +0530

    implemented handleIncAmountClick and handleResetClick func to dispatch value from reducer func by using useState to handle amount value and print payload data by button click

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,40 @@
 import { useDispatch, useSelector } from 'react-redux'
 import './App.css'
-import { decrement, increment } from './features/couter/couterSlice';
+import { decrement, increment, incrementByAmount, reset } from './features/couter/couterSlice';
+import { useState } from 'react';
 
 function App() {
+  const [amount, setAmount] = useState(0);
+  // used useSelector hook to take value from state
   const count = useSelector((state) => state.counter.value);
+  // used useDispatch to dispatch value from state 
   const dispatch = useDispatch();
   function handleIncrementClick(){
+    // used reducer function to count value by 1 
     dispatch(increment());
   }
   function handleDecrementClick(){
+    // used reducer function to decrease count by 1
     dispatch(decrement());
   }
+  function handleResetClick(){
+    dispatch(reset());
+  }
+  function handleIncAmountClick(){
+    dispatch(incrementByAmount(amount));
+  }
+
   return (
     <>
      <div className='container'>
         <button onClick={handleIncrementClick}> + </button>
         <p>Count: {count}</p>
         <button onClick={handleDecrementClick}> - </button>
+        <br /><br />
+        <button onClick={handleResetClick}> Reset </button>
+        <input type="number" value={amount} placeholder='Enter Amount' onChange={(e) =>setAmount(e.target.value)}/>
+        <br /><br />
+        <button onClick={handleIncAmountClick}> Inc by Amount </button>
      </div>
     </>
   )
