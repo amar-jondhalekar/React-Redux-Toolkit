af9c0a355d7fac7cdc730c6b73dc722f52dd01b8
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-11-05 10:08:26 +0530

    to imported the reducer function from the counter slice and add it to our store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from '../features/counter/counterSlice'
+import counterReducer from '../features/couter/couterSlice'
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
 })
+// Steps: 
+// create store
+// wrap app component under Provider
+// create Slice 
+// register reducer in store  
